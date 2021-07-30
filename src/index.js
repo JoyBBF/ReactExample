7d404bd8f9d41f7facb122828ab2cb3daa509745
Joy Bonilla <47568038+JoyBBF@users.noreply.github.com>
2021-07-30 16:57:21 -0600

    Finished base example

--- src/index.js
+++ src/index.js
@@ -14,5 +14,29 @@ function Item(){
 
 ReactDOM.render(
     <h1>Hello World</h1>,
+    const [word, setWord] = useState("");
+
+    return (
+    <div>
+        <div><h1>React Example</h1></div>
+        <div>
+            <input type="text" onChange={ (e) => {handleChange(e.target.value); setWord(e.target.value);}} placeholder="Insert text"></input>
+            <p>{word}</p>
+        </div>
+        <div id="rows">
+            <h3>Changes:</h3>
+        </div>
+    </div>
+    );
+}
+
+function handleChange(value){
+    let div = document.createElement("div");
+    div.innerHTML = value;
+    document.getElementById("rows").appendChild(div);
+}
+
+ReactDOM.render(
+    <Item />,
     document.getElementById('app')
 );
