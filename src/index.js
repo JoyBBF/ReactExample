ad1a5fe9e473b345bd844dd8b37c1a1fe36fed09
Joy Bonilla <47568038+JoyBBF@users.noreply.github.com>
2021-07-30 16:59:21 -0600

    Updated dist and fixes index

--- src/index.js
+++ src/index.js
@@ -2,18 +2,6 @@ import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
 function Item(){
-    const [count, setCount] = useState(0);
-
-    return <>
-    <p>{count}</p>
-    <button onClick={ () => setCount(count + 1)}>
-        +
-    </button>
-    </>
-}
-
-ReactDOM.render(
-    <h1>Hello World</h1>,
     const [word, setWord] = useState("");
 
     return (
